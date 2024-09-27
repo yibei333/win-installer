202b1f6171410521ff64569c95a5a1cef083d08f
yibei333 <Developer333@hotmail.com>
2024-09-27 18:38:40 +0800

    add ui

--- src/WinInstaller/MainWindow.xaml.cs
+++ src/WinInstaller/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,49 @@ namespace WinInstaller
     /// </summary>
     public partial class MainWindow : Window
     {
+        public MainViewModel ViewModel { get; set; } = new MainViewModel();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            if (ViewModel.Running)
+            {
+                MessageBox.Show("任务处理中,请稍候...");
+                e.Cancel = true;
+            }
+        }
+
+        private void Window_Minimal(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Window_Close(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        bool _running;
+
+        [ObservableProperty]
+        string _name;
+
+        [RelayCommand]
+        public async Task Change()
+        {
+            Running = true;
+            await Task.Yield();
+            await Task.Delay(5000);
+            Name += "Changed";
+            Running = false;
         }
     }
 }
